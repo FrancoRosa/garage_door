25a3321aa15900dcafcb626c0f772d8ebcdce91a
FrancoRosa <km115.franco@gmail.com>
2021-07-03 23:28:16 -0500

    Add flag structure

--- garage_door.ino
+++ garage_door.ino
@@ -1,3 +1,6 @@
+// Software serial to stablish communication by bluetooth
+#include <SoftwareSerial.h>
+
 // Sensor inputs to know if the doors are fully open or closed
 #define s1_cls  2
 #define s1_opn  3
@@ -9,12 +12,30 @@
 #define doorlock 7
 
 // Motor driver
-#define en1     A0
-#define en2     A1
-#define dir1_a  9
-#define dir1_b  10
-#define dir2_a  11
-#define dir2_b  12
+#define en1     8
+#define en2     9
+#define dir1_a  A0
+#define dir1_b  A1
+#define dir2_a  A2
+#define dir2_b  A3
+
+SoftwareSerial SerialBT(11, 12); // RX, TX
+
+bool flag_open= false;
+bool flag_close = false;
+bool flag_stop = false;
+
+void open_door() {
+
+}
+
+void close_door() {
+  
+}
+
+void stop_door() {
+  
+}
 
 void pinConfig() {
   pinMode(s1_cls, INPUT_PULLUP);
@@ -33,10 +54,38 @@ void pinConfig() {
   pinMode(dir2_b, OUTPUT);
 }
 
+void serialConfig() {
+  Serial.begin(115200);
+  SerialBT.begin(9600);
+}
+
+void processCommand() {
+  char c = SerialBT.read();
+  Serial.print(c);
+  flag_open = flag_close = flag_stop = false;
+  switch (c) {
+    case 'o':
+      flag_open = true; break;
+    case 'c':
+      flag_close = true; break;
+    case 's':
+      flag_stop = true; break;
+    default:
+      break;
+  }
+}
+
 void setup() {
   pinConfig();
+  serialConfig();
 }
 
 void loop() {
-  
+  if (SerialBT.available()) {
+    processCommand();
+  }
+
+  if (flag_open) open_door(); 
+  if (flag_close) close_door(); 
+  if (flag_stop) stop_door(); 
 }
